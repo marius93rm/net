d29a21c96ea42fbec9bf84e4cea91f16f5cf6a44
marius93rm <marius93rm@gmail.com>
2022-11-03 11:55:16 +0100

    done

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+
+import { actionCreators, reducer, initialState } from './posts';
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      dispatch(actionCreators.loading());
+
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await response.json();
+        dispatch(actionCreators.success(posts));
+      } catch (error) {
+        dispatch(actionCreators.failure());
+      }
+    fetchPosts();
+    }
+  }, []);
+
+  const { loading, error, posts } = state;
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" animating={true} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text>Something went wrong</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      style={styles.container}
+      keyExtractor={(post) => post.id}
+      data={posts}
+      renderItem={
+        ( {item: {id, title, body}}, index ) => (
+          <View key={id} style={styles.post}>
+            <Text style={styles.title}>{index}. {title}</Text>
+            <Text style={styles.body}>{body}</Text>
+          </View>
+        )
+        }
+      />
+
   );
 }
 
@@ -17,4 +64,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  post: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  body: {
+    fontSize: 14
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
---
+++ posts.js
@@ -0,0 +1,29 @@
+const types = {
+    LOADING: 'LOADING',
+    SUCCESS: 'SUCCESS',
+    FAILURE: 'FAILURE'
+}
+
+export const actionCreators = {
+    loading: () => ({ type: types.LOADING }),
+    failure: () => ({ type: types.FAILURE }),
+    success: (payload) => ({ type: types.SUCCESS, payload })
+}
+
+export const initialiState = {
+    loading: true,
+    error: false,
+    posts: []
+}
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case types.LOADING:
+            return { ...state, loading: true }
+        case types.SUCCESS:
+            return { ...state, loading: false, posts: action.payload }
+        case types.FAILURE:
+            return { ...state, loading: false, error: true }
+    }
+}
+
